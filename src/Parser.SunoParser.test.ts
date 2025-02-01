7f899e5bab85e6774b7a5d82df9f0d418ddf6ca8
8beeeaaat(sadao.komaki) <8beeeaaat@gmail.com>
2025-02-01 12:44:26 +0900

    initial commit

---
+++ src/Parser.SunoParser.test.ts
@@ -0,0 +1,50 @@
+/**
+ * @vitest-environment jsdom
+ */
+
+import path from "node:path";
+import { LineArgsTokenizer } from "@ioris/tokenizer-kuromoji";
+import { type IpadicFeatures, type Tokenizer, builder } from "kuromoji";
+import { beforeEach, describe, expect, it } from "vitest";
+import sample from "../sample.json";
+import { SunoParser } from "./";
+
+const kuromojiBuilder = builder({
+  dicPath: path.resolve(__dirname, "../node_modules/kuromoji/dict"),
+});
+
+const getTokenizer = (): Promise<Tokenizer<IpadicFeatures>> =>
+  new Promise((resolve, reject) => {
+    kuromojiBuilder.build((err, tokenizer) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(tokenizer);
+    });
+  });
+
+describe("SunoParser", () => {
+  let parser: SunoParser;
+
+  beforeEach(async () => {
+    const tokenizer = await getTokenizer();
+
+    parser = new SunoParser({
+      tokenizer: (lineArgs) =>
+        LineArgsTokenizer({
+          lineArgs,
+          tokenizer,
+        }),
+    });
+  });
+
+  it("should parse syllable Suno lyric", async () => {
+    const result = await parser.parse(
+      sample,
+      "7fcebd38-00b7-4c9a-9eba-1df4af210e48",
+    );
+    expect(result.resourceID).toBe("7fcebd38-00b7-4c9a-9eba-1df4af210e48");
+    expect(result.duration).toBe(0);
+    expect(result.voids().length).toBe(13);
+  }, 10000);
+});
