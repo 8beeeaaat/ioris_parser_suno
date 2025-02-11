b3893f99332dc19a38eb8c36eec99d7604c747c7
8beeeaaat(sadao.komaki) <8beeeaaat@gmail.com>
2025-02-11 15:49:29 +0900

    0.1.2

--- src/Parser.SunoParser.test.ts
+++ src/Parser.SunoParser.test.ts
@@ -45,6 +45,6 @@ describe("SunoParser", () => {
     );
     expect(result.resourceID).toBe("7fcebd38-00b7-4c9a-9eba-1df4af210e48");
     expect(result.duration).toBe(0);
-    expect(result.voids().length).toBe(17);
+    expect(result.voids().length).toBe(12);
   }, 10000);
 });
