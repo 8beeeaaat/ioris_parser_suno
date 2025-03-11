500b65a22886ec773804fcc8e2707d24c5c12d05
8beeeaaat(sadao.komaki) <8beeeaaat@gmail.com>
2025-03-11 20:05:01 +0900

    0.1.5

--- src/Parser.SunoParser.ts
+++ src/Parser.SunoParser.ts
@@ -1,10 +1,4 @@
-import {
-  type LineCreateArgs,
-  Lyric,
-  type LyricCreateArgs,
-  type ParagraphCreateArgs,
-  type WordCreateArgs,
-} from "@ioris/core";
+import { Lyric, type LyricCreateArgs, type WordTimeline } from "@ioris/core";
 
 export type SunoChar = {
   word: string;
@@ -23,6 +17,118 @@ type sunoWord = sunoChar[];
 type sunoLine = sunoWord[];
 type sunoParagraph = sunoLine[];
 
+function parseLineTimelines(line: sunoLine) {
+  const wordTimelines = [];
+
+  for (const word of Array.from(line)) {
+    if (!word.length) {
+      continue;
+    }
+    const { wordTimeline } = parseWordTimelines(word);
+    wordTimelines.push(wordTimeline);
+  }
+
+  return {
+    wordTimelines,
+  };
+}
+
+function parseWordTimelines(word: sunoWord) {
+  const joinedText = word.reduce<string>(
+    (acc, char) => `${acc}${char.hasWhitespace ? `${char.word} ` : char.word}`,
+    "",
+  );
+  const text = joinedText
+    // 複数行にまたがる括弧で囲まれた部分を削除
+    .replace(/【[\s\S]*?】/g, "")
+    .replace(/\[[\s\S]*?\]/g, "")
+    .replace(/\([\s\S]*?\)/g, "")
+    .replace(/（[\s\S]*?）/g, "")
+    // 複数の空白を1つにまとめる
+    .replace(/[^\S\r\n]+/g, " ")
+    .trim();
+  const wordTimeline: WordTimeline = {
+    wordID: crypto.randomUUID(),
+    begin: word[0].start_s,
+    end: word[word.length - 1].end_s,
+    hasNewLine: word.reduce<boolean>(
+      (acc, char) => acc || char.hasNewline,
+      false,
+    ),
+    hasWhitespace: word.reduce<boolean>(
+      (acc, char) => acc || char.hasWhitespace,
+      false,
+    ),
+    text,
+  };
+  return { wordTimeline };
+}
+
+function parseParagraphTimelines(paragraph: sunoParagraph) {
+  const lineTimelines = [];
+
+  for (const line of Array.from(paragraph)) {
+    const { wordTimelines } = parseLineTimelines(line);
+    lineTimelines.push(wordTimelines);
+  }
+
+  return {
+    lineTimelines,
+  };
+}
+
+export function SunoCharsToLyricCreateArgs(chars: SunoChar[]) {
+  const timelines = [];
+  const paragraphs = chars.reduce<sunoParagraph[]>(
+    (acc, char, index) => {
+      const newChar = { ...char, hasWhitespace: false, hasNewline: false };
+      const nextChar = chars[index + 1];
+
+      const lastParagraphIndex = acc.length ? acc.length - 1 : 0;
+      const lastLineIndex = acc[lastParagraphIndex].length
+        ? acc[lastParagraphIndex].length - 1
+        : 0;
+      const lastWordIndex = acc[lastParagraphIndex][lastLineIndex].length
+        ? acc[lastParagraphIndex][lastLineIndex].length - 1
+        : 0;
+
+      newChar.word = newChar.word.trimStart();
+      if (newChar.word.startsWith("\n")) {
+        newChar.word = newChar.word.replaceAll("\n", "");
+      }
+      if (newChar.word.endsWith("\n\n")) {
+        nextChar ? acc.push([[[]]]) : null;
+        newChar.word = newChar.word.slice(0, -2);
+      }
+      if (newChar.word.endsWith("\n")) {
+        nextChar ? acc[acc.length - 1].push([[]]) : null;
+        newChar.word = newChar.word.slice(0, -1);
+        newChar.hasNewline = true;
+      }
+      if (newChar.word.endsWith("　")) {
+        newChar.word = newChar.word.slice(0, -1);
+        newChar.hasNewline = true;
+      }
+      if (newChar.word.endsWith(" ")) {
+        newChar.word = newChar.word.slice(0, -1);
+        newChar.hasWhitespace = true;
+      }
+
+      acc[lastParagraphIndex][lastLineIndex][lastWordIndex].push(newChar);
+
+      return acc;
+    },
+    [[[[]]]],
+  );
+
+  for (const paragraph of Array.from(paragraphs)) {
+    const { lineTimelines } = parseParagraphTimelines(paragraph);
+    timelines.push(lineTimelines);
+  }
+
+  return timelines;
+}
+
 export class SunoParser {
   tokenizer?: LyricCreateArgs["tokenizer"];
   offsetSec?: number;
@@ -36,54 +142,7 @@ export class SunoParser {
   }
 
   public async parse(chars: SunoChar[], resourceID: string) {
-    const timelines: LyricCreateArgs["timelines"] = [];
-    const paragraphs = chars.reduce<sunoParagraph[]>(
-      (acc, char, index) => {
-        const newChar = { ...char, hasWhitespace: false, hasNewline: false };
-        const nextChar = chars[index + 1];
-
-        const lastParagraphIndex = acc.length ? acc.length - 1 : 0;
-        const lastLineIndex = acc[lastParagraphIndex].length
-          ? acc[lastParagraphIndex].length - 1
-          : 0;
-        const lastWordIndex = acc[lastParagraphIndex][lastLineIndex].length
-          ? acc[lastParagraphIndex][lastLineIndex].length - 1
-          : 0;
-
-        newChar.word = newChar.word.trimStart();
-        if (newChar.word.startsWith("\n")) {
-          newChar.word = newChar.word.replaceAll("\n", "");
-        }
-        if (newChar.word.endsWith("\n\n")) {
-          nextChar ? acc.push([[[]]]) : null;
-          newChar.word = newChar.word.slice(0, -2);
-        }
-        if (newChar.word.endsWith("\n")) {
-          nextChar ? acc[acc.length - 1].push([[]]) : null;
-          newChar.word = newChar.word.slice(0, -1);
-          newChar.hasNewline = true;
-        }
-        if (newChar.word.endsWith("　")) {
-          newChar.word = newChar.word.slice(0, -1);
-          newChar.hasNewline = true;
-        }
-        if (newChar.word.endsWith(" ")) {
-          newChar.word = newChar.word.slice(0, -1);
-          newChar.hasWhitespace = true;
-        }
-
-        acc[lastParagraphIndex][lastLineIndex][lastWordIndex].push(newChar);
-
-        return acc;
-      },
-      [[[[]]]],
-    );
-
-    for (const paragraph of Array.from(paragraphs)) {
-      const { lineTimelines } = this.parseParagraphTimelines(paragraph);
-      timelines.push(lineTimelines);
-    }
-
+    const timelines = SunoCharsToLyricCreateArgs(chars);
     const lyric = await new Lyric({
       resourceID,
       duration: 0,
@@ -93,72 +152,6 @@ export class SunoParser {
     }).init();
 
     return lyric;
-  }
-
-  private parseParagraphTimelines(paragraph: sunoParagraph): {
-    lineTimelines: ParagraphCreateArgs["timelines"];
-  } {
-    const lineTimelines: ParagraphCreateArgs["timelines"] = [];
-
-    for (const line of Array.from(paragraph)) {
-      const { wordTimelines } = this.parseLineTimelines(line);
-      lineTimelines.push(wordTimelines);
-    }
-
-    return {
-      lineTimelines,
-    };
-  }
-
-  private parseLineTimelines(line: sunoLine): {
-    wordTimelines: LineCreateArgs["timelines"];
-  } {
-    const wordTimelines: LineCreateArgs["timelines"] = [];
-
-    for (const word of Array.from(line)) {
-      if (!word.length) {
-        continue;
-      }
-      const { wordTimeline } = this.parseWordTimelines(word);
-      wordTimelines.push(wordTimeline);
-    }
-
-    return {
-      wordTimelines,
-    };
-  }
-
-  private parseWordTimelines(word: sunoWord): {
-    wordTimeline: WordCreateArgs["timeline"];
-  } {
-    const joinedText = word.reduce<string>(
-      (acc, char) =>
-        `${acc}${char.hasWhitespace ? `${char.word} ` : char.word}`,
-      "",
-    );
-    const text = joinedText
-      // 複数行にまたがる括弧で囲まれた部分を削除
-      .replace(/【[\s\S]*?】/g, "")
-      .replace(/\[[\s\S]*?\]/g, "")
-      .replace(/\([\s\S]*?\)/g, "")
-      .replace(/（[\s\S]*?）/g, "")
-      // 複数の空白を1つにまとめる
-      .replace(/[^\S\r\n]+/g, " ")
-      .trim();
-    const wordTimeline: WordCreateArgs["timeline"] = {
-      begin: word[0].start_s,
-      end: word[word.length - 1].end_s,
-      hasNewLine: word.reduce<boolean>(
-        (acc, char) => acc || char.hasNewline,
-        false,
-      ),
-      hasWhitespace: word.reduce<boolean>(
-        (acc, char) => acc || char.hasWhitespace,
-        false,
-      ),
-      text,
-    };
-    return { wordTimeline };
   }
 }
 
